255b7902e55344da612bf966f61d599feb7c6bfd
tripletm26 <46800783+tripletm26@users.noreply.github.com>
2019-12-06 08:46:51 -0500

    debug false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%=_ldwfetvmr)%@mzb49aimv&zi$n2-$-zs@wyhnm54b#f-(46'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
